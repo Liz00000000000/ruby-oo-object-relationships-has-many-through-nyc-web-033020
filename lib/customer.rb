fca5fb75afae14d4b1632535d6523be324ce2ff9
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-04-02 19:22:58 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
 class Customer
+    attr_reader :name, :tip
+    attr_writer :age
+    @@all = [ ]
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @tip = tip 
+      @@all << self
+    end
   
+    def self.all
+      @@all
+    end
+  
+    def new_meal(waiter, total, tip = 0)
+      Meal.new(waiter, self, total, tip)
+    end
+  
+    def meals
+      Meal.all.select do |meal|
+        meal.customer == self
+      end
+    end
+  
+    def waiters
+      Meal.all.map do |meals|
+            if meals.customer == self
+              meals.waiter 
+            end 
+        end.compact!
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,46 @@
+require 'pry'
 class Waiter
+    attr_reader :name, :customer
+    attr_writer :years_of_experience
+    @@all = [ ]
+      def initialize(name, years_of_experience)
+        @name = name
+        @years_of_experience = years_of_experience
+        @customer = customer
+        @@all << self
+      end
+    
+      def self.all
+        @@all
+      end
+    
+      def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+      end
+    
+      def meals
+        Meal.all.select do |meal|
+          meal.waiter == self
+        end
+      end
+    
+       def best_tip
+        tips = [ ]
+        Meal.all.each do |meal|
+            tips << meal.tip 
+          end 
+        return tips.max 
+        end 
 
+
+      def best_tipper
+        Meal.all.each do |meals|
+            #binding.pry
+            if meals.tip == best_tip 
+            return meals.customer 
+            end 
+            #this did not work with .select 
+            #why is that? 
+        end 
+      end
 end
